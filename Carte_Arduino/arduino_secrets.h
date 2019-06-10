68f7aeb771e31ba1286a60fafa30970c3df48668
CodingForward <gabriel.lemagicien@gmail.com>
2019-06-10 16:53:09 +0200

    Update arduino_secrets.h

--- Carte_Arduino/arduino_secrets.h
+++ Carte_Arduino/arduino_secrets.h
@@ -2,15 +2,15 @@
 
 #ifdef MAISON
 
-  #define SECRET_SSID "FREEBOX_CAROLINE_YK"
-  #define SECRET_PASS "24D4A41972"
+  #define SECRET_SSID "BOX_DE_LA_MAISON"
+  #define SECRET_PASS "MOT_DE_PASSE_DE_LA_MAISON"
   #define WEP
   #define KEY_INDEX 1
 
 #else
 
-  #define SECRET_SSID "Jojo"
-  #define SECRET_PASS "SwordArtOnline"
+  #define SECRET_SSID "NOM_DU_PARTAGE_DE_CONNEXION"
+  #define SECRET_PASS "MOT_DE_PASSE_DU_PARTAGE_DE_CONNEXION"
   #define WPA
 
 #endif
